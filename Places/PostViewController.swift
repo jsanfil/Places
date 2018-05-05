40f766d022061aef53d873ae29159592c0c31da3
Joe Sanfilippo <jsanfil@gmail.com>
2018-05-05 10:51:25 -0700

    Added camera and photo library to select image for post.

--- Places/PostViewController.swift
+++ Places/PostViewController.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 import Cosmos
+import os.log
 
-class PostViewController: UITableViewController {
+// TODO:
+// - convert images to Kingfisher
+// - create an image name so that we can post the image to S3
 
+class PostViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    // Post Data
     var place = RPlace()
+
+    var imagePicked: UIImage?
+    var imagePicker = UIImagePickerController()
+    
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var authorField: UITextField!
     @IBOutlet weak var ratingField: CosmosView!
@@ -22,27 +32,29 @@ class PostViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         debugPrint("SettingsViewController viewDidLoad():", place)
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        imagePicker.delegate = self
     }
 
+    // Action Sheet that allows user to take photo or pic from photo library
     @IBAction func imageAction(_ sender: Any) {
         // create an actionSheet
         let actionSheetController: UIAlertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         
-        // create an action
-        let firstAction: UIAlertAction = UIAlertAction(title: "First Action", style: .default) { action -> Void in
-            
-            print("First Action pressed")
+        // create the actions
+        let firstAction: UIAlertAction = UIAlertAction(title: "Take Picture or Video", style: .default) { action -> Void in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                self.imagePicker.sourceType = .camera;
+                self.imagePicker.allowsEditing = false
+                self.present(self.imagePicker, animated: true, completion: nil)
+            }
         }
         
-        let secondAction: UIAlertAction = UIAlertAction(title: "Second Action", style: .default) { action -> Void in
-            
-            print("Second Action pressed")
+        let secondAction: UIAlertAction = UIAlertAction(title: "Photo Library", style: .default) { action -> Void in
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                self.imagePicker.sourceType = .photoLibrary;
+                self.imagePicker.allowsEditing = true
+                self.present(self.imagePicker, animated: true, completion: nil)
+            }
         }
         
         let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .cancel) { action -> Void in }
@@ -55,6 +67,13 @@ class PostViewController: UITableViewController {
         // present an actionSheet...
         present(actionSheetController, animated: true, completion: nil)
     }
+    
+    // Delegate function: handles a selected image from camera or photo library
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imagePicked = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismiss(animated:true, completion: nil)
+    }
+
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         updateModelData()
@@ -82,6 +101,12 @@ class PostViewController: UITableViewController {
         ratingField.rating = place.rating ?? 3.0
         categoryField.text = place.category ?? "Select category"
         sharingField.text = place.access ?? "Select mode"
+        if let myImage = imagePicked {
+            imageButton.setImage(myImage, for: .normal)
+        } else {
+            let imageName = place.defaultImage ?? "defaultPhoto"
+            imageButton.setImage(UIImage(named: imageName), for: .normal)
+        }
         print("Rating", place.rating)
         print("Place", place.toJSONString())
 
@@ -93,5 +118,6 @@ class PostViewController: UITableViewController {
         place.rating = ratingField.rating
         place.category = categoryField.text
         place.access = sharingField.text
+        // image name gets set when you change the image in the image button action
     }
 }
